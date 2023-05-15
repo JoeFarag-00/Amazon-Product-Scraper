c07e0f3fdeed184bddc6a0de80e79900d911ef6a
Youssef Raghed Farag <88057098+JoeFarag-00@users.noreply.github.com>
2023-05-15 04:55:23 +0300

    Awaiting Product_Name

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import csv
 from selenium.webdriver.common.by import By
 from tkinter import *
 import time
+from selenium.common.exceptions import NoSuchElementException
 
 def Scrape_Amazon():
     keyword = keyword_entry.get()
@@ -11,26 +12,53 @@ def Scrape_Amazon():
     driver = webdriver.Chrome(options=options)
     driver.get(f'https://www.amazon.co.uk/s?k={keyword}')
     
-    product_elements = driver.find_elements(By.CSS_SELECTOR, 'div[data-component-type="s-search-result"]')
     ct = 0
     time.sleep(1)
     
+    product_elements = driver.find_elements(By.CSS_SELECTOR, 'div[data-component-type="s-search-result"]')
+ 
     products = []
     for element in product_elements:
         try:
-            product_page = element.find_element(By.CSS_SELECTOR, 'a.a-link-normal.s-underline-text.s-underline-link-text.s-link-style.a-text-normal').get_attribute('href')
-            product_name = element.find_element(By.CSS_SELECTOR, 'span.a-size-medium.a-color-base.a-text-normal').text
-            price = element.find_element(By.CSS_SELECTOR, 'span.a-price-whole').text
-            rating = element.find_element(By.CSS_SELECTOR, 'span[aria-label*=" out of 5 stars"]').get_attribute('aria-label').split(' out of 5 stars')[0]
-            product_image = element.find_elements(By.CSS_SELECTOR, 'div[data-component-type="s-search-result"] img')[0].get_attribute('src')
+            try:
+                product_page = element.find_element(By.CSS_SELECTOR, 'a.a-link-normal.s-underline-text.s-underline-link-text.s-link-style.a-text-normal').get_attribute('href')
+            except NoSuchElementException:
+                product_page = "Lost"
             
+            try:
+                product_name = element.find_element(By.CSS_SELECTOR, 'span.a-size-medium.a-color-base.a-text-normal').text
+            except NoSuchElementException:
+                product_name = "Lost"
+            
+            try:
+                price = element.find_element(By.CSS_SELECTOR, 'span.a-price-whole').text
+            except NoSuchElementException:
+                price = "Lost"
+            
+            try:
+                rating = element.find_element(By.CSS_SELECTOR, 'span[aria-label*=" out of 5 stars"]').get_attribute('aria-label').split(' out of 5 stars')[0]
+            except NoSuchElementException:
+                rating = "Lost"
+            # company_name = element.find_element(By.CSS_SELECTOR, 'div[data-component-type="s-search-result"] span.a-size-small.a-color-secondary.a-text-normal').text.split('by ')[-1]# NOT Scrapable
+            try:
+                product_image = element.find_elements(By.CSS_SELECTOR, 'div[data-component-type="s-search-result"] img')[0].get_attribute('src')
+            except NoSuchElementException:
+                product_image = "Lost"
+                
             products.append([product_page, product_name, price, rating, product_image])
         except:
             ct+=1
-
+    
     print("Failed Scrapes: ", ct)
-    for product in products:
-        print(product)
+    
+    print(products)
+
+    driver.quit()
+
+    with open('products.csv', 'a', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Product Page', 'Product Name','Price', 'Ratings', 'Product Image'])
+        writer.writerows(products)
 
 root = Tk()
 root.title('Amazon Scraper')
